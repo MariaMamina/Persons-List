f60f5b698ec6484ced5e0ee6480bbddcd636224c
Maria Mamina <maria.mamina3@gmail.com>
2021-02-17 11:40:56 +0200

    First tasks realisation

---
+++ Persons List/DetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsViewController.swift
+//  Persons List
+//
+//  Created by Maria Mamina on 17.02.2021.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    var person: Person!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        phoneLabel.text = "Phone: \(person.phoneNumber)"
+        emailLabel.text = "Email: \(person.email)"
+        navigationItem.title = person.fulName
+
+    }
+  
+}
